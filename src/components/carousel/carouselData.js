ce6116c8e8aa55f030031426d3e31a400d55beba
ejpelagio <pelagio.erikj@gmail.com>
2021-01-17 17:54:57 -0600

    Imgage object for fences

---
+++ src/components/carousel/carouselData.js
@@ -0,0 +1,71 @@
+import Fence1 from "../../assets/images/fenceFix/fence1fix.png";
+import Fence3 from "../../assets/images/fenceFix/fence3fix.png";
+import Fence4 from "../../assets/images/fenceFix/fence4fix.png";
+import Fence5 from "../../assets/images/fenceFix/fence5fix.png";
+import Fence6 from "../../assets/images/fenceFix/fence6fix.png";
+import Fence7 from "../../assets/images/fenceFix/fence7fix.png";
+import Fence8 from "../../assets/images/fenceFix/fence8fix.png";
+import Fence9 from "../../assets/images/fenceFix/fence9fix.png";
+import Fence10 from "../../assets/images/fenceFix/fence10fix.png";
+import Fence11 from "../../assets/images/fenceFix/fence11fix.png";
+import Fence12 from "../../assets/images/fenceFix/fence12fix.png";
+
+export const CarouselData = {
+  fences: [
+    {
+      name: "fenceDoor",
+      image: Fence1,
+      alt: "Fence with Door",
+    },
+    {
+      name: "fancyDoor",
+      image: Fence3,
+      alt: "Fence with molding",
+    },
+    {
+      name: "doubleFence",
+      image: Fence4,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence5,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence6,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence7,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence8,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence9,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence10,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence11,
+      alt: "Double Sided Fence",
+    },
+    {
+      name: "doubleFence",
+      image: Fence12,
+      alt: "Double Sided Fence",
+    },
+  ],
+};
