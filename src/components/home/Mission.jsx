df74a898319cb484291942077d44dac6cae489ab
ejpelagio <pelagio.erikj@gmail.com>
2021-01-17 21:52:27 -0600

    adding coments and div for phonenumber

--- src/components/home/Mission.jsx
+++ src/components/home/Mission.jsx
@@ -1,16 +1,17 @@
+// importing dependencies
 import React from "react";
 import styled from "styled-components/macro";
 import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
-
-import WhatsAppLogo from "../../assets/images/WhatsApp.jpeg";
-import TextMessageLogo from "../../assets/images/textMess.png";
-import "./home.css";
-
 import Row from "react-bootstrap/Row";
 
+// Importing local imgages
+import WhatsAppLogo from "../../assets/images/whatsApp.png";
+import TextMessageLogo from "../../assets/images/textMess.png";
+
+// create the companys mission component and style using styled-components
 const Mission = () => {
   return (
+    // bootstrap container
     <Container>
       <Row lg={1} sm={1}>
         <CompanyName>Elias Fencing Services</CompanyName>
@@ -31,60 +32,62 @@ const Mission = () => {
           <br />
           and much more.
         </ServicesParagraph>
-        <p style={{ textAlign: "center", paddingTop: ".8rem" }}>
-          Send me a message for a free estimate!
-        </p>
+        <SendMeAMessage>Send me a message for a free estimate!</SendMeAMessage>
       </Row>
+      {/* creating the button div and creatin button that sends a text message onPress*/}
       <ButtonDiv>
-        <Image
+        <StyledIcon
           onClick={(e) => {
-            console.log("Image click");
             window.open("sms:12514875008&body=HiElias", "_blank");
           }}
-          width={100}
-          height={80}
-          className="buttonCall"
           src={TextMessageLogo}
           roundedCircle
         />
 
-        <Image
+        {/* creating button that sends a whatsApp message onPress*/}
+        <StyledIcon
           onClick={(e) => {
-            console.log("Image click");
             window.open(
               "https://api.whatsapp.com/send?phone=12514875008",
               "_blank"
             );
           }}
-          width={100}
-          height={80}
           src={WhatsAppLogo}
           roundedCircle
         />
       </ButtonDiv>
+      {/* create a phone component with an a tag to make a phone call*/}
+      <PhoneNumber>
+        <PhoneNumberLink href="tel:2514875008">(251) 487-5008</PhoneNumberLink>
+      </PhoneNumber>
     </Container>
   );
 };
 
 export default Mission;
 
+// Styling using styled components
 const CompanyName = styled.h1`
   font-size: 36px;
   text-align: center;
   padding-top: 3rem;
   padding-bottom: 2rem;
   color: rgb(182, 61, 50);
+  width: 100%;
 `;
+
 const MissionParagraph = styled.div`
   font-size: 20px;
   text-align: center;
   line-height: 1.2;
 `;
+
 const ServicesTitle = styled.div`
   font-size: 28px;
   text-align: center;
   padding-top: 5rem;
   padding-bottom: 2rem;
+  width: 100%;
 `;
 
 const ServicesParagraph = styled.div`
@@ -92,8 +95,38 @@ const ServicesParagraph = styled.div`
   text-align: center;
   line-height: 1.4;
 `;
+
 const ButtonDiv = styled.div`
   display: flex;
   justify-content: center;
   padding: 2rem;
+`;
+
+const SendMeAMessage = styled.p`
+  text-align: "center";
+  padding: 0.8rem 0;
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const StyledIcon = styled.img`
+  width: 100px;
+  background-color: transparent;
+  cursor: pointer;
+  margin: 0 10px;
+`;
+
+const PhoneNumber = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const PhoneNumberLink = styled.a`
+  font-size: 3rem;
+  text-decoration: none;
+  color: black;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
 `;
